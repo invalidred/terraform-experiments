d1cb032a897e49fb014b4e24e94e0abb6589e84c
invalidred <abdulask@gmail.com>
2020-04-19 09:45:17 -0400

    feat(aws): s3 & elastic ip dependencies on ec2 instance

--- aws-instance/example.tf
+++ aws-instance/example.tf
@@ -6,5 +6,15 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+  depends_on    = [aws_s3_bucket.example]
 }
 
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.example.id
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "abdul-khan-terraform-example-bucket"
+  acl    = "private"
+}
